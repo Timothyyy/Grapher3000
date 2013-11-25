0bb50ce4565798b8af19f001a0ef7bcbd4d3ad54
Timothy <2546382@gmail.com>
2013-11-25 14:00:05 +0300

    Painting
    Drawing of verteces and adding them into list of verteces functions
    added.

--- mainunit.pas
+++ mainunit.pas
@@ -26,6 +26,8 @@ type
     DeleteVertex: TToolButton;
     DeleteEdge: TToolButton;
     procedure FormCreate(Sender: TObject);
+    procedure GraphMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
   private
     { private declarations }
     Edges : TList;
@@ -48,12 +50,29 @@ var
   temp : TVertex;
 begin
   Verteces := TList.Create;
+  Graph.Canvas.Rectangle(Graph.BoundsRect);
+  Graph.Canvas.Clear;
+  //Graph.Canvas.Ellipse(10,25,30,45);
+  //Graph.Canvas.TextOut(17,27,'1');
   {temp := TVertex.Create(1, 5, 5);
-  Vertces.Add(temp);
+  Verteces.Add(temp);
   temp := TVertex.Create(2, 6, 6);
-  Vertces.Add(temp);
+  Verteces.Add(temp);
   Verteces.Delete(0);
   ShowMessage(IntToStr(TVertex(Verteces[0]).Id));}
+end;
+
+procedure TMainForm.GraphMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if AddVertex.Down then
+    begin
+      Graph.Canvas.Ellipse(X - 10, Y - 10, X + 10, Y + 10);
+      Graph.Canvas.TextOut(X - 6, Y - 8, IntToStr(Verteces.Count + 1));
+      Verteces.Add(TVertex.Create(Verteces.Count + 1, X, Y));
+    end;
+  if AddEdge.Down then
+    ShowMessage(IntToStr(Verteces.Count));
 end;
 
 end.
